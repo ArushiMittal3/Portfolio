a9ece81b83333685684259cf923a0a4622b12eda
Arushi Mittal <arushimittal2432@gmail.com>
2025-06-22 19:53:28 +0530

    Hero section + Bento Grid added

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,18 @@
-import Hero from "@/components/Hero";
+import Hero from '@/components/Hero'
+import { FloatingNav } from '@/components/ui/FloatingNavBar'
+import Grid from '@/components/ui/Grid'
+import { FaHome } from 'react-icons/fa'
 
-export default function Home() {
+export default function Home () {
   return (
-    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5 min-h-screen">
-      <div className="max-w-7xl w-full">
-        <Hero/>
+    <main className='relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5'>
+      <div className='max-w-7xl w-full'>
+        <FloatingNav
+          navItems={[{ name: 'Home', link: '/', icon: <FaHome /> }]}
+        />
+        <Hero />
+        <Grid/>
       </div>
     </main>
-  );
+  )
 }
